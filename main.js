9a8f8a5487ec83921b9e8247a6481e488776616a
JFerrettUK <jferrettuk@gmail.com>
2023-03-08 09:17:26 +0000

    recursion function to loop through list + prepend

--- main.js
+++ main.js
@@ -5,28 +5,47 @@ class LinkedList {
     }
 
     append = function(value) {
-        this.tail = new ListNode(value);
+        let newNode = new ListNode(value)
+        //find first Node  where 'next == null' and point to newNode
+
+        let newList = loopToEnd(list.head, newNode)
+
+        //insert new node as property of list
+        this.size = this.size + 1
     }
 
     prepend = function(value) {
-        //make the head of the list point to the new new node
         let newNode = new ListNode(value)
-        console.log(newNode)
-        this.head = newNode.value;
-
-        this.size++
+        if (this.head) {
+            newNode.next = this.head
+        }
+        this.head = newNode;
+        this.size = this.size + 1    
     }
 }
 
 class ListNode {
-    constructor(value, next = null) {
-        this.value = value;
+    constructor(data, next = null) {
+        this.data = data;
         this.next = next;
     }
 }
 
+let loopToEnd = function(value, newNode) {
+    if (value.next == null) {
+        value.next = newNode
+        console.log(value)
+        return value
+    }
+    console.log(value)
+    loopToEnd(value.next)
+}
+
 let list = new LinkedList()
 
-console.log(list)
 list.prepend(1)
-console.log(list)
+list.prepend(5)
+list.prepend(7)
+list.append(9)
+
+
